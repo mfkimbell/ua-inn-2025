fa92f0e31c96e0b755c42d6ede6f00e6f95f1457
Jayden Pyles <jpylesbusiness@gmail.com>
2025-03-29 14:18:35 -0500

    feat: add routers

--- src/backend/app.py
+++ src/backend/app.py
@@ -3,7 +3,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from backend.auth.router import router as auth_router
+from backend.order.router import router as order_router
+from backend.request.router import router as request_router
 from backend.startup import on_startup
+from backend.suggestion.router import router as suggestion_router
 
 _ = load_dotenv()
 
@@ -19,7 +22,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 on_startup()
+
+app.include_router(order_router)
+app.include_router(request_router)
+app.include_router(suggestion_router)
 
 
 @app.get("/health")
