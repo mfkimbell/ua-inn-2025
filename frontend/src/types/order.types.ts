73ff843bc92b2d8877121cc0b8e8a560d5a5ab8f
Jayden Pyles <jpylesbusiness@gmail.com>
2025-03-29 13:11:11 -0500

    feat: add server types

---
+++ frontend/src/types/order.types.ts
@@ -0,0 +1,17 @@
+export type ServerOrder = {
+  id: number;
+  user_id: number;
+  status: string;
+  created_at: string;
+  updated_at: string;
+  completed_at: string;
+};
+
+export type Order = {
+  id: number;
+  userId: number;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  completedAt: string;
+};
